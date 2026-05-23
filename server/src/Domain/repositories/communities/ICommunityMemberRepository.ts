794e6767f705dc7d145d9633de90287bf32e8853
Ignjat Radojicic <ignjatradojicic@gmail.com>
2026-05-23 19:35:13 +0200

    Update ICommunityMemberRepository signatures
    - Update addMember and updateMemberRole signatures to take community role string ('moderator' | 'member') instead of UserRole enum

--- server/src/Domain/repositories/communities/ICommunityMemberRepository.ts
+++ server/src/Domain/repositories/communities/ICommunityMemberRepository.ts
@@ -1,6 +1,7 @@
 import { Community } from "../../models/Community";
 import { CommunityMember } from "../../models/CommunityMember";
 import { UserRole } from "../../enums/UserRole";
+import { CommunityRole } from '../../../Domain/enums/CommunityRole';
 
 export interface ICommunityMemberRepository
 {
@@ -8,8 +9,8 @@ export interface ICommunityMemberRepository
       getMember(userId: number, communityId: number): Promise<CommunityMember | null>;
       getMembers(communityId:number): Promise<CommunityMember[]>;
       getMemberUserIds(communityId:number): Promise<number[]>;
-      addMember(userId: number, communityId: number, userRole:UserRole, status:string): Promise<boolean>;
-      updateMemberRole(userId:number,communityId:number,userRole:UserRole) : Promise<boolean>;
+      addMember(userId: number, communityId: number, role: CommunityRole, status: string): Promise<boolean>
+      updateMemberRole(userId: number, communityId: number, role: CommunityRole): Promise<boolean>
       updateMemberStatus(userId:number,communityId:number, status:string) : Promise<boolean>;
       removeMember(userId:number,communityId:number): Promise<boolean>;
 
