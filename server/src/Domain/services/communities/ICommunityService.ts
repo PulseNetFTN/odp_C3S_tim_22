02ee22bd1e78ec319b02b91fca4fe58ad26ef9b8
Ignjat Radojicic <ignjatradojicic@gmail.com>
2026-05-23 20:26:40 +0200

    refactor: add searchCommunities to ICommunityService interface

--- server/src/Domain/services/communities/ICommunityService.ts
+++ server/src/Domain/services/communities/ICommunityService.ts
@@ -1,17 +1,25 @@
-import { CommunityDto } from "../../DTOs/community/CommunityDto";
-import { CommunityMemberDto } from "../../DTOs/community/CommunityMemberDto";
-import { CreateCommunityInput, DeleteCommunityInput, GetCommunityByIdInput, GetUserCommunitiesInput, JoinCommunityInput, LeaveCommunityInput, UpdateCommunityInput } from "../../types/inputs/CommunityInputs";
-import { ServiceResult } from "../../types/ServiceResult";
+import { CommunityDto } from '../../DTOs/community/CommunityDto';
+import { ServiceResult } from '../../types/ServiceResult';
+import {
+    CreateCommunityInput,
+    GetCommunityByIdInput,
+    GetUserCommunitiesInput,
+    SearchCommunitiesInput,
+    UpdateCommunityInput,
+    DeleteCommunityInput,
+    JoinCommunityInput,
+    LeaveCommunityInput,
+} from '../../types/inputs/CommunityInputs';
 
 export interface ICommunityService {
-    createCommunity(input:CreateCommunityInput): Promise<ServiceResult<CommunityDto>>;
+    createCommunity(input: CreateCommunityInput): Promise<ServiceResult<CommunityDto>>;
     getCommunityById(input: GetCommunityByIdInput): Promise<ServiceResult<CommunityDto>>;
     getAllCommunities(): Promise<ServiceResult<CommunityDto[]>>;
     getPublicCommunities(): Promise<ServiceResult<CommunityDto[]>>;
     getUserCommunities(input: GetUserCommunitiesInput): Promise<ServiceResult<CommunityDto[]>>;
+    searchCommunities(input: SearchCommunitiesInput): Promise<ServiceResult<CommunityDto[]>>;
     updateCommunity(input: UpdateCommunityInput): Promise<ServiceResult<CommunityDto>>;
-    deleteCommunity(input:DeleteCommunityInput): Promise<ServiceResult<boolean>>;
+    deleteCommunity(input: DeleteCommunityInput): Promise<ServiceResult<boolean>>;
     joinCommunity(input: JoinCommunityInput): Promise<ServiceResult<boolean>>;
-    leaveCommunity(input:LeaveCommunityInput): Promise<ServiceResult<boolean>>;
-   
+    leaveCommunity(input: LeaveCommunityInput): Promise<ServiceResult<boolean>>;
 }
