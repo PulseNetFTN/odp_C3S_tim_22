58e37cdd8efd6617e7d714c9d5c93ee3b80ac05a
Ignjat Radojicic <ignjatradojicic@gmail.com>
2026-05-23 20:26:41 +0200

    refactor: add createTag to ITagService, update import path for TagDto

--- server/src/Domain/services/tags/ITagService.ts
+++ server/src/Domain/services/tags/ITagService.ts
@@ -1,11 +1,10 @@
-import { TagDto } from "../../DTOs/users/TagDto";
-import { ServiceResult } from "../../types/ServiceResult";
-import { DeleteTagInput, UpdateTagInput } from "../../types/inputs/TagInputs";
+import { TagDto } from '../../DTOs/tags/TagDto';
+import { ServiceResult } from '../../types/ServiceResult';
+import { CreateTagInput, UpdateTagInput, DeleteTagInput } from '../../types/inputs/TagInputs';
 
 export interface ITagService {
+    createTag(input: CreateTagInput): Promise<ServiceResult<TagDto>>;
     getAllTags(): Promise<ServiceResult<TagDto[]>>;
     updateTag(input: UpdateTagInput): Promise<ServiceResult<boolean>>;
     deleteTag(input: DeleteTagInput): Promise<ServiceResult<boolean>>;
 }
- 
- 
